28d130ac2a54280212012d48041dc0d243825aeb
Ravi Tailor <ravi.tailor@timesinternet.in>
2018-06-24 15:06:54 +0530

    Write test case for ListViewController

--- FlickerSampleTests/ListViewControllerTest.swift
+++ FlickerSampleTests/ListViewControllerTest.swift
@@ -39,18 +39,38 @@ class ListViewControllerTest: XCTestCase {
         super.tearDown()
     }
     
-    func testPaging() {
-        for var i in 1 ... 3 {
-            let count = (self.listViewController.photos.count - (self.listViewController.photos.count/2))
-            let indexPath = IndexPath(row: (8*i), section: 0)
-           self.listViewController.loadNextPageData(indexPath, isInProcess:false, count: count)
-            wait(for: [], timeout: 10.0)
-            i += 1
-            if i > 3 {
-                print("total count",self.listViewController.photos.count)
-            }
-        }
-    }
+//    func testPaging() {
+//        for var i in 1 ... 3 {
+//            let count = (self.listViewController.photos.count - (self.listViewController.photos.count/2))
+//            let indexPath = IndexPath(row: (8*i), section: 0)
+//            self.listViewController.loadNextPageData(indexPath, isInProcess:false, count: count)
+//            // Create an expectation for a background download task.
+//            let expectation = XCTestExpectation(description: "Download data from flicker")
+//
+//            let urlStr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=2&safe_search=1&text=kittens&per_page=100&page=" + String(indexPath.row)
+//            // Create a URL for a web page to be downloaded.
+//            let url = URL(string: urlStr)!
+//
+//            // Create a background task to download the web page.
+//            let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+//
+//                // Make sure we downloaded some data.
+//                XCTAssertNotNil(data, "No data was downloaded.")
+//
+//                // Fulfill the expectation to indicate that the background task has finished successfully.
+//                expectation.fulfill()
+//
+//            }
+//
+//            // Start the download task.
+//            dataTask.resume()
+//            wait(for: [expectation], timeout: 10.0)
+//            i += 1
+//            if i > 3 {
+//                print("total count",self.listViewController.photos.count)
+//            }
+//        }
+//    }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
